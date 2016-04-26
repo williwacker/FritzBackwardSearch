9e3f0c7d89f03b6652ac7b11437c7ec0df1ffd15
Werner Kuehn <werner_kuehn@gmx.de>
2016-04-26 15:24:18 +0200

    Update install.sh
    make sed statement more generic

--- install.sh
+++ install.sh
@@ -2,8 +2,8 @@
 
 locationOfScript=$(dirname "$(readlink -e "$0")")
 
-sed -i "s|/home/fritzbox|$locationOfScript|" fritzCallMon.sh
-sed -i "s|/home/fritzbox|$locationOfScript|" fritzCallMon
+sed -i "s|INSTDIR=.*|INSTDIR=$locationOfScript|" fritzCallMon.sh
+sed -i "s|INSTDIR=.*|INSTDIR=$locationOfScript|" fritzCallMon
 
 mv fritzCallMon /etc/init.d/fritzCallMon
 chmod +x /etc/init.d/fritzCallMon
