d9607425626065454371738f32a5b5ba48939882
Werner Kühn <werner_kuehn@gmx.de>
2021-04-16 14:29:00 +0200

    add try/except block

--- fritzWLAN.py
+++ fritzWLAN.py
@@ -29,16 +29,20 @@ class FritzWLANStatus(object):
 		return wlan
 
 	def get_active_devices(self):
-		online_status = {}
-		hosts = [host['mac']
-				 for host in FritzWLAN(self.connection, service=1).get_hosts_info() if host['status'] == True]  # 2.4GHz
-		hosts += [host['mac']
-				  for host in FritzWLAN(self.connection, service=2).get_hosts_info() if host['status'] == True]  # 5GHz
-		hosts += [host['mac']
-				  for host in FritzWLAN(self.connection, service=3).get_hosts_info() if host['status'] == True]  # GuestAccount
-		for name, mac in self.wlan_prefs.items():
-			online_status[name] = 1 if mac in hosts else 0
-		# send as mqtt message
-		cmd = 'mosquitto_pub -d -h homematic-raspi -t server/fritz/maconline/'
-		for name, status in online_status.items():
-			os.system("{}{} -m {}".format(cmd, name, status))
+		try:
+			online_status = {}
+			hosts = [host['mac']
+					for host in FritzWLAN(self.connection, service=1).get_hosts_info() if host['status'] == True]  # 2.4GHz
+			hosts += [host['mac']
+					for host in FritzWLAN(self.connection, service=2).get_hosts_info() if host['status'] == True]  # 5GHz
+			hosts += [host['mac']
+					for host in FritzWLAN(self.connection, service=3).get_hosts_info() if host['status'] == True]  # GuestAccount
+			for name, mac in self.wlan_prefs.items():
+				online_status[name] = 1 if mac in hosts else 0
+			# send as mqtt message
+			logger.info(online_status)
+			cmd = 'mosquitto_pub -d -h homematic-raspi -t server/fritz/maconline/'
+			for name, status in online_status.items():
+				os.system("{}{} -m {}".format(cmd, name, status))
+		except Exception as e:
+			logger.error("Cannot post MQTT request: {}".format(e))
