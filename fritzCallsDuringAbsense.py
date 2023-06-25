e4705bdae8f1df593166e8218ec3d97801823ecf
Werner Kühn <werner_kuehn@gmx.de>
2023-06-25 13:39:42 +0200

    add pushover support and speech2text

--- fritzCallsDuringAbsense.py
+++ fritzCallsDuringAbsense.py
@@ -1,13 +1,17 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import http.client
 import logging
 import os
 import re
+import urllib
 from datetime import datetime
 
+import requests
 import urllib3
 import xmltodict
+import speech_recognition as sr
 
 logger = logging.getLogger(__name__)
 
@@ -63,7 +67,8 @@ class FritzCallsDuringAbsense():
 					callentry['Caller'] = self.get_fullCode(callentry['Caller'])
 					phone_message = self.get_phone_message(callentry)  # if callentry['Port'] in ('40') else ""
 					logger.info("phone_message="+str(phone_message))
-					self.put_telegram_message(callentry, phone_message)
+#                    self.telegram(self.get_message(callentry, phone_message))
+					self.pushover(self.get_message(callentry, phone_message))
 				return
 
 	def get_phone_message(self, callentry):
@@ -80,21 +85,45 @@ class FritzCallsDuringAbsense():
 			wave = open(os.path.join(self.prefs['phone_msg_dir'], '{}.wav'.format(dlfile[-1])), 'wb')
 			wave.write(response.data)
 			wave.close()
-			phone_message = "{}".format(dlfile[-1])
+			phone_message = self.speech_to_text(wave.name)
 		return phone_message
 
-	def put_telegram_message(self, callentry, phone_message):
+#    def telegram(self, message):
+#        self.http.headers = {'Content-type': 'application/json'}
+#        try:
+#            self.http.request('GET', 'https://api.telegram.org/bot{}/sendMessage?chat_id={}\&text={}'.format(
+#                self.prefs['telegram_token'], self.prefs['telegram_chat_id'], message), timeout=4.0, retries=False)
+#        except Exception as e:
+#           logger.error(e)
+
+	def telegram(self, bot_message):
+		url = f"https://api.telegram.org/bot{self.prefs['telegram_token']}/sendMessage"
+		parms = {'chat_id': self.prefs['telegram_chat_id'], 'text': bot_message}
+		requests.get(url, parms, timeout=4)
+
+	def pushover(self, message):
+		if self.prefs['pushover_token'] and self.prefs['pushover_userkey']:
+			conn = http.client.HTTPSConnection("api.pushover.net:443")
+			conn.request("POST", "/1/messages.json",
+						 urllib.parse.urlencode({
+							 "token": self.prefs['pushover_token'],
+							 "user": self.prefs['pushover_userkey'],
+							 "message": message,
+						 }), {"Content-type": "application/x-www-form-urlencoded"})
+			conn.getresponse()
+
+	def get_message(self, callentry, phone_message):
 		if callentry['Caller'] is None:
 			callentry['Caller'] = ""
 		if callentry['Name'] is None:
 			callentry['Name'] = ""
 		calltime = datetime.strptime(callentry['Date'], '%d.%m.%y %H:%M').strftime("%d.%m.%Y %H:%M")
 		if phone_message:
-			text = '{0} {1} {2} /getab{3}'.format(
+			text = '{0} {1} {2} /Message: {3}'.format(
 				calltime,
 				callentry['Name'].encode('utf-8').decode('utf-8'),
 				callentry['Caller'],
-				phone_message.split('.')[2]
+				phone_message
 			)
 		else:
 			text = '{} {} {}'.format(
@@ -102,9 +131,15 @@ class FritzCallsDuringAbsense():
 				callentry['Name'].encode('utf-8').decode('utf-8'),
 				callentry['Caller']
 			)
-		self.http.headers = {'Content-type': 'application/json'}
-		try:
-			self.http.request('GET', 'https://api.telegram.org/bot{}/sendMessage?chat_id={}\&text={}'.format(
-				self.prefs['telegram_token'], self.prefs['telegram_chat_id'], text), timeout=4.0, retries=False)
-		except Exception as e:
-			logger.error(e)
+		return text
+
+	def speech_to_text(self, filename):
+		# initialize the recognizer
+		r = sr.Recognizer()
+		# open the file
+		with sr.AudioFile(filename) as source:
+			# listen for the data (load audio to memory)
+			audio_data = r.record(source)
+			# recognize (convert from speech to text)
+			text = r.recognize_google(audio_data, language="de_DE")
+			return text
